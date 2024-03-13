7c92920e064a68fc9ec14b1a98cf7e98dfe9b6a2
Мирослав <Murukvolodumur@gmail.com>
2024-03-13 14:17:18 +0200

    виконав завдання 1

---
+++ EX1/EX1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int lastDigit = 3;
+
+        Console.Write("Enter the first number: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter another number: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the third number: ");
+        int num3 = int.Parse(Console.ReadLine());
+
+        int startInterval = 1;
+        int endInterval = 10 + lastDigit;
+
+        Console.WriteLine("Numbers that lie on the interval [{0}, {1}]:", startInterval, endInterval);
+        if (num1 >= startInterval && num1 <= endInterval)
+            Console.WriteLine(num1);
+        if (num2 >= startInterval && num2 <= endInterval)
+            Console.WriteLine(num2);
+        if (num3 >= startInterval && num3 <= endInterval)
+            Console.WriteLine(num3);
+
+        Console.ReadKey();
+    }
+}
