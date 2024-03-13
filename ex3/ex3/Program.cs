3bed2b7b6d1da2f17a1e4c53a2fad8b795da6127
Мирослав <Murukvolodumur@gmail.com>
2024-03-13 14:18:35 +0200

    Виконав завдання 3

---
+++ ex3/ex3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int lastDigit = 3;
+
+        int arrayLength = 10 + lastDigit;
+
+        int[] X = new int[arrayLength];
+        Random random = new Random();
+        for (int i = 0; i < arrayLength; i++)
+        {
+            X[i] = random.Next(100);
+        }
+
+        int minValue = X[0];
+        int maxValue = X[0];
+        for (int i = 1; i < arrayLength; i++)
+        {
+            if (X[i] < minValue)
+            {
+                minValue = X[i];
+            }
+            else if (X[i] > maxValue)
+            {
+                maxValue = X[i];
+            }
+        }
+
+        Console.Write("Array X: ");
+        foreach (int value in X)
+        {
+            Console.Write(value + " ");
+        }
+        Console.WriteLine();
+
+        Console.WriteLine($"Minimum value: {minValue}");
+        Console.WriteLine($"Maximum value: {maxValue}");
+    }
+}
