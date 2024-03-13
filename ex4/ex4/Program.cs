be68636d00b85841c0bfb9bfbaa498fa06a56fb7
Мирослав <Murukvolodumur@gmail.com>
2024-03-13 14:19:39 +0200

    виконав завдання 4

---
+++ ex4/ex4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int lastDigit = 3;
+
+        int arrayLength = 10 + lastDigit;
+
+        int[] X = new int[arrayLength];
+        Random random = new Random();
+        for (int i = 0; i < arrayLength; i++)
+        {
+            X[i] = random.Next(-99, 100);
+        }
+
+        Console.Write("Enter the number M: ");
+        int M = int.Parse(Console.ReadLine());
+
+        int[] Y = Array.FindAll(X, x => Math.Abs(x) > Math.Abs(M));
+
+        Console.Write("Array X: ");
+        foreach (int value in X)
+        {
+            Console.Write(value + " ");
+        }
+        Console.WriteLine();
+
+        Console.WriteLine($"Number M: {M}");
+        Console.Write("Array Y: ");
+        foreach (int value in Y)
+        {
+            Console.Write(value + " ");
+        }
+        Console.WriteLine();
+    }
+}
